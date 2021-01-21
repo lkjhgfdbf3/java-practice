bca2d00537dd6b1cd0fb0ac55e5cd98344bc9c3e
lkjhgfdbf3 <96kdg@naver.com>
2021-01-22 00:22:55 +0900

    if문과 BufferReader을 이용하여 a와 b 비교하기

---
+++ src/day210122/BackJoon1330.java
@@ -0,0 +1,21 @@
+package day210122;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BackJoon1330 {
+	public static void main(String[] args) throws IOException {
+		InputStream inputstream = System.in;
+		InputStreamReader isr = new InputStreamReader(inputstream);
+		BufferedReader bfr = new BufferedReader(isr);
+		
+		String str = bfr.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if(a>b) System.out.println(">");
+		else if(a<b) System.out.println("<");
+		else System.out.println("=");
+	}
+}
