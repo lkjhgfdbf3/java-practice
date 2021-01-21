110983883e1d523e8f67164de225bcb68752a807
lkjhgfdbf3 <96kdg@naver.com>
2021-01-22 02:39:39 +0900

    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램/while문

---
+++ src/day210122/BackJoon10952.java
@@ -0,0 +1,20 @@
+package day210122;
+import java.util.Scanner;
+
+public class BackJoon10952 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			int a = scanner.nextInt(); 
+			int b = scanner.nextInt();
+		
+			if(a==0 && b==0) {
+				break;
+			}
+			System.out.println(a+b);
+		}
+		scanner.close();
+	}
+
+}
