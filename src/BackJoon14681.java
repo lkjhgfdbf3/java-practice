92ccc56419fd9ff0e7740c90c573ec002884c887
lkjhgfdbf3 <96kdg@naver.com>
2021-01-21 17:01:03 +0900

    주어진 점이 어느 사분면에 속하는지 알아내는 것

---
+++ src/BackJoon14681.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BackJoon14681 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		if(A>0 && B>0) {
+			System.out.println("Quadrant 1");
+		}
+		else if(A<0 && B>0) {
+			System.out.println("Quadrant 2");
+		}
+		else if(A<0 && B<0) {
+			System.out.println("Quadrant 3");
+		}
+		else {
+			System.out.println("Quadrant 4");
+		}
+	}
+}
