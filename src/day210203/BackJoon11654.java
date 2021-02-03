a1a1abe4d4b11d139e0636f6337ebfdf32d11da0
lkjhgfdbf3 <96kdg@naver.com>
2021-02-03 23:31:48 +0900

    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력

---
+++ src/day210203/BackJoon11654.java
@@ -0,0 +1,9 @@
+package day210203;
+import java.io.*;
+
+public class BackJoon11654 {
+	public static void main(String[] args) throws IOException {
+		int a = System.in.read();
+		System.out.println(a);
+	}
+}
