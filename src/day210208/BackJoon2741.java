8aadf3bacd9f3e2b4c3eae1f4d7253c4353a1e85
lkjhgfdbf3 <96kdg@naver.com>
2021-02-08 23:07:25 +0900

    자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오

---
+++ src/day210208/BackJoon2741.java
@@ -0,0 +1,13 @@
+package day210208;
+import java.io.*;
+
+public class BackJoon2741 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(bfr.readLine());
+		
+		for(int i=1; i<=a; i++) {
+			System.out.println(i);
+		}
+	}
+}
