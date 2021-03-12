e2058dfdf7d1a10b5976b08be97278f970b7a71a
lkjhgfdbf3 <96kdg@naver.com>
2021-03-13 02:56:26 +0900

    -

--- src/day210312/BaekJoon1789.java
+++ src/day210312/BaekJoon1789.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BaekJoon1789 {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
-        long n = Long.parseLong(br.readLine());
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long S = Long.parseLong(br.readLine());
 
-        System.out.println(solve(n));
+        System.out.println(solve(S));
     }
 
     static int solve(long n) {
@@ -19,7 +17,6 @@ public class BaekJoon1789 {
         while(n >= sum) {
             sum += addNum++;
         }
-
         return sum == n ? addNum - 1 : addNum - 2;
     }
 }
