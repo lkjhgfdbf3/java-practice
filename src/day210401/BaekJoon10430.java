f91221513ba001187804b855ce89418824ac66a7
lkjhgfdbf3 <96kdg@naver.com>
2021-04-01 14:01:35 +0900

    나머지

---
+++ src/day210401/BaekJoon10430.java
@@ -0,0 +1,16 @@
+package day210401;
+import java.util.*;
+
+public class BaekJoon10430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a =sc.nextInt();
+		int b =sc.nextInt();
+		int c =sc.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+	}
+}
