3c597335caaa69adb5b099c8ec599ff9f1ce060d
lkjhgfdbf3 <96kdg@naver.com>
2021-04-06 14:09:04 +0900

    이친수

--- src/day210406/BaekJoon2193.java
+++ src/day210406/BaekJoon2193.java
@@ -1,26 +1,20 @@
 package day210406;
 import java.util.*;
+import java.math.*;
 
 public class BaekJoon2193 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int [][]d = new int[n+1][2];
-		int []ans = new int[n+1];
-		d[1][0]=0;
-		d[1][1]=1;
-		
-		for(int i=2; i<=n; i++) {
-			for(int j=0; j<=1; j++) {
-				if(j==0) {
-					d[n][0]=d[n-1][0]+d[n-1][1];
-				}
-				if(j==1) {
-					d[n][1]=d[n-1][0];
-				}
-				ans[i] = d[n][0]+d[n][1];
-			}
+		long []d = new long[n+1];
+		d[1]=1;
+
+		if(n>=2) {
+			d[2]=1;
 		}
-		System.out.println(ans[n]);
+		for(int i=3; i<=n; i++) {
+				d[i] = d[i-1]+d[i-2];
+		}
+		System.out.println(d[n]);
 	}
 }
