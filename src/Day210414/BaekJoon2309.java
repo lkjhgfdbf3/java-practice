329228efd07aa056106564cec6f8c0fdaa749ac6
lkjhgfdbf3 <96kdg@naver.com>
2021-04-14 16:51:03 +0900

    일곱난쟁이

---
+++ src/Day210414/BaekJoon2309.java
@@ -0,0 +1,28 @@
+package Day210414;
+import java.util.*;
+
+public class BaekJoon2309 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int []a = new int[9];
+		int sum=0;
+		
+		for(int i=0; i<9; i++) {
+			a[i]=sc.nextInt();
+			sum += a[i];
+		}
+		Arrays.sort(a);
+		for(int i=0; i<9; i++) {
+			for(int j=i+1; j<9; j++) {
+				if(sum-a[i]-a[j]==100) {
+					for(int k=0; k<9; k++) {
+						if(k==i && k==j) 
+							continue;
+						System.out.println(a[k]);
+					}
+					System.exit(0);
+				}
+			}
+		}
+	}
+}
