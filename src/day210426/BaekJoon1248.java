d9a68de5496941bb22ed3985f4ff726c263f538a
lkjhgfdbf3 <96kdg@naver.com>
2021-04-26 17:40:41 +0900

    맞춰봐 시간복잡도 21^10

--- src/day210426/BaekJoon1248.java
+++ src/day210426/BaekJoon1248.java
@@ -1,10 +1,65 @@
 package day210426;
+import java.util.*;
 
 public class BaekJoon1248 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int n;
+	static int[][]sign;
+	static int[]ans;
+	public static boolean ok() {
+		for(int i=0; i<n; i++) {
+			int sum=0;
+			for(int j=i; j<n; j++) {
+				sum +=ans[j];
+				if(sign[i][j]==0) {
+					if(sum!=0) return false;
+				}
+				else if(sign[i][j]>0) {
+					if(sum<=0) return false;
+				}
+				else if(sign[i][j]<0) {
+					if(sum>=0) return false;
+				}
+			}
+		}
+		return true;
 	}
-
+	public static boolean go(int index) {
+		if(index==n) {
+			return ok();
+		}
+		for(int i=-10; i<=10; i++) {
+			ans[index]=i;
+			if(go(index+1)) return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		ans=new int[n];
+		sign = new int[n][n];
+		String s = sc.next();
+		int cnt = 0;
+		
+		for(int i=0; i<n; i++) {
+			for(int j=i; j<n; j++) {
+				char x = s.charAt(cnt);
+				if(x=='0') {
+					sign[i][j]=0;
+				}
+				else if(x=='+') {
+					sign[i][j]=1;
+				}
+				else if(x=='-') {
+					sign[i][j]=-1;
+				}
+				cnt+=1;
+			}
+		}
+		go(0);
+		for(int i=0; i<n; i++) {
+			System.out.print(ans[i]+" ");
+		}
+		System.out.println();
+	}
 }
